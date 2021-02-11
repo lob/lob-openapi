c4342b6a5c31452b0de71f8b187def532815b53b
Hilary J Holz <holz.hilary@gmail.com>
2021-02-11 10:39:48 -0800

    prettify

--- tests/addresses_test.js
+++ tests/addresses_test.js
@@ -1,67 +1,67 @@
-'use strict'
+"use strict";
 
 // standard setup, present in every test
-const test = require('tape')
-const Prism = require('./setup.js')
+const test = require("tape");
+const Prism = require("./setup.js");
 
 // test specific data
-const resource_endpoint = '/addresses',
-  lobUri = 'https://api.lob.com/v1',
-  specFile = './Lob-API-public.yml'
+const resource_endpoint = "/addresses",
+  lobUri = "https://api.lob.com/v1",
+  specFile = "./Lob-API-public.yml";
 
-const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN)
+const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN);
 
 // contract tests
-test('list addresses', async function (t) {
+test("list addresses", async function (t) {
   const response = await prism
     .setup()
     .then((client) =>
       client.get(resource_endpoint, { headers: prism.authHeader })
-    )
+    );
 
-  t.equal(response.status, 200)
-})
+  t.equal(response.status, 200);
+});
 
-test('create, retrieve, then delete an address', async function (t) {
+test("create, retrieve, then delete an address", async function (t) {
   const params = {
-    description: 'Harry - Office',
-    name: 'Harry Zhang',
-    company: 'Lob',
-    email: 'harry@lob.com',
-    phone: '5555555555',
-    address_line1: '185 Berry St',
-    address_line2: '# 6100',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94107',
-    address_country: 'US',
-  }
+    description: "Harry - Office",
+    name: "Harry Zhang",
+    company: "Lob",
+    email: "harry@lob.com",
+    phone: "5555555555",
+    address_line1: "185 Berry St",
+    address_line2: "# 6100",
+    address_city: "San Francisco",
+    address_state: "CA",
+    address_zip: "94107",
+    address_country: "US",
+  };
 
   let response = await prism
     .setup()
     .then((client) =>
       client.post(resource_endpoint, params, { headers: prism.authHeader })
-    )
+    );
 
-  t.equal(response.status, 200)
+  t.equal(response.status, 200);
 
-  const adr_id = response.data.id
+  const adr_id = response.data.id;
 
   // retrieve
   response = await prism.setup().then((client) =>
     client.get(`${resource_endpoint}/${adr_id}`, {
       headers: prism.authHeader,
     })
-  )
+  );
 
-  t.equal(response.status, 200)
+  t.equal(response.status, 200);
 
   // delete
   response = await prism.setup().then((client) =>
     client.delete(`${resource_endpoint}/${adr_id}`, {
       headers: prism.authHeader,
     })
-  )
+  );
 
-  t.equal(response.status, 200)
-})
+  t.equal(response.status, 200);
+});
--- tests/zip_lookups_test.js
+++ tests/zip_lookups_test.js
@@ -1,17 +1,17 @@
-'use strict'
+"use strict";
 
-const test = require('tape')
-const Prism = require('./setup.js')
+const test = require("tape");
+const Prism = require("./setup.js");
 
 // test specific data
-const resource_endpoint = '/us_zip_lookups',
-  zip = '94107',
-  lobUri = 'https://api.lob.com/v1',
-  specFile = './Lob-API-public.yml'
+const resource_endpoint = "/us_zip_lookups",
+  zip = "94107",
+  lobUri = "https://api.lob.com/v1",
+  specFile = "./Lob-API-public.yml";
 
-const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN)
+const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN);
 
-test('lookup a US zip code', async function (t) {
+test("lookup a US zip code", async function (t) {
   const response = await prism
     .setup()
     .then((client) =>
@@ -20,23 +20,23 @@ test('lookup a US zip code', async function (t) {
         { zip_code: zip },
         { headers: prism.authHeader }
       )
-    )
+    );
 
-  t.equal(response.status, 200)
-})
+  t.equal(response.status, 200);
+});
 
-test('use an incorrectly formatted zip code', async function (t) {
+test("use an incorrectly formatted zip code", async function (t) {
   const response = await prism
     .setup()
     .then((client) =>
       client.post(
         resource_endpoint,
-        { zip_code: '123456' },
+        { zip_code: "123456" },
         { headers: prism.authHeader }
       )
-    )
+    );
 
-  t.equal(response.status, 422)
-  t.match(response.data.error.message, /zip_code/)
-  t.end()
-})
+  t.equal(response.status, 422);
+  t.match(response.data.error.message, /zip_code/);
+  t.end();
+});

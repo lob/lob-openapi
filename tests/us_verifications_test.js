f14c1d614e53025b35d2c826b8cd6c309beab40d
Aditi <aditi@lob.com>
2021-02-10 18:37:39 -0800

    did some formatting changes

--- tests/us_verifications_test.js
+++ tests/us_verifications_test.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict'
 
-const test = require("tape");
-const Prism = require("./setup.js");
+const test = require('tape')
+const Prism = require('./setup.js')
 
-const resource_endpoint = "/us_verifications",
-  lobUri = "https://api.lob.com/v1",
-  specFile = "./Lob-API-public.yml";
-const primary_line = "185 BERRY ST";
-const city = "SAN FRANCISCO";
-const state = "CA";
-const zip_code = "94107";
-const sla = "185 BERRY ST 94107";
+const resource_endpoint = '/us_verifications',
+  lobUri = 'https://api.lob.com/v1',
+  specFile = './Lob-API-public.yml'
+const primary_line = '185 BERRY ST'
+const city = 'SAN FRANCISCO'
+const state = 'CA'
+const zip_code = '94107'
+const sla = '185 BERRY ST 94107'
 
-const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN);
+const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN)
 
-test("verify a US address given primary line, city, and state", async function (t) {
+test('verify a US address given primary line, city, and state', async function (t) {
   const response = await prism
     .setup()
     .then((client) =>
@@ -23,12 +23,12 @@ test("verify a US address given primary line, city, and state", async function (
         { primary_line: primary_line, city: city, state: state },
         { headers: prism.authHeader }
       )
-    );
+    )
 
-  t.equal(response.status, 200);
-});
+  t.equal(response.status, 200)
+})
 
-test("verify a US address given primary line and zip code", async function (t) {
+test('verify a US address given primary line and zip code', async function (t) {
   const response = await prism
     .setup()
     .then((client) =>
@@ -37,12 +37,12 @@ test("verify a US address given primary line and zip code", async function (t) {
         { primary_line: primary_line, zip_code: zip_code },
         { headers: prism.authHeader }
       )
-    );
+    )
 
-  t.equal(response.status, 200);
-});
+  t.equal(response.status, 200)
+})
 
-test("verify a US address given a single-line address", async function (t) {
+test('verify a US address given a single-line address', async function (t) {
   const response = await prism
     .setup()
     .then((client) =>
@@ -51,12 +51,12 @@ test("verify a US address given a single-line address", async function (t) {
         { address: sla },
         { headers: prism.authHeader }
       )
-    );
+    )
 
-  t.equal(response.status, 200);
-});
+  t.equal(response.status, 200)
+})
 
-test("errors when given a primary line without city/state or zip", async function (t) {
+test('errors when given a primary line without city/state or zip', async function (t) {
   const response = await prism
     .setup({ errors: false })
     .then((client) =>
@@ -65,14 +65,14 @@ test("errors when given a primary line without city/state or zip", async functio
         { primary_line: primary_line },
         { headers: prism.authHeader }
       )
-    );
+    )
 
-  t.equal(response.status, 422);
-  t.match(response.data.error.message, /zip_code/);
-  t.end();
-});
+  t.equal(response.status, 422)
+  t.match(response.data.error.message, /zip_code/)
+  t.end()
+})
 
-test("errors when not given a primary line", async function (t) {
+test('errors when not given a primary line', async function (t) {
   const response = await prism
     .setup({ errors: false })
     .then((client) =>
@@ -81,14 +81,14 @@ test("errors when not given a primary line", async function (t) {
         { zip_code: zip_code },
         { headers: prism.authHeader }
       )
-    );
+    )
 
-  t.equal(response.status, 422);
-  t.match(response.data.error.message, /primary_line/);
-  t.end();
-});
+  t.equal(response.status, 422)
+  t.match(response.data.error.message, /primary_line/)
+  t.end()
+})
 
-test("errors when given extraneous information alongside a single-line address", async function (t) {
+test('errors when given extraneous information alongside a single-line address', async function (t) {
   const response = await prism
     .setup({ errors: false })
     .then((client) =>
@@ -97,9 +97,9 @@ test("errors when given extraneous information alongside a single-line address",
         { address: sla, zip_code: zip_code },
         { headers: prism.authHeader }
       )
-    );
+    )
 
-  t.equal(response.status, 422);
-  t.match(response.data.error.message, /zip_code/);
-  t.end();
-});
+  t.equal(response.status, 422)
+  t.match(response.data.error.message, /zip_code/)
+  t.end()
+})

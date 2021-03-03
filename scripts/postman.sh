967b1636f4d65ec2793d274c5ccbeabfeaea4c4c
Christina Jin <christina.jin@lob.com>
2021-03-03 16:26:52 -0600

    Update npm postman pkg name to openapi-to-postmanv2

--- scripts/postman.sh
+++ scripts/postman.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 make bundle
 
-npx openapi2postmanv2 --pretty --spec dist/Lob-API-public-bundled.yml --output dist/Lob-API-postman.txt
+npx openapi-to-postmanv2 --pretty --spec dist/Lob-API-public-bundled.yml --output dist/Lob-API-postman.txt

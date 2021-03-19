232a9cb7b15e52ce977b8c6770fe651397195dbe
Hilary J Holz <holz.hilary@gmail.com>
2021-03-19 15:23:06 -0700

    add tracking events

--- tests/letters_test.js
+++ tests/letters_test.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// TODO:
+// Need to add subscription features to the lob open api token used to
+// run CI and contract tests in order to cancel, use certified, etc.
+
 // standard setup, present in every test
 const test = require("tape");
 const Prism = require("./setup.js");
@@ -12,20 +16,61 @@ const resource_endpoint = "/letters",
 const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN);
 
 // contract tests happy path
-test("create, list, read, replace, update, then delete a letter", async function (t) {
-  // create
+test("create, list, read then cancel a letter", async function (t) {
+  // create to and from addresses to use in create
   let response = await prism.setup().then((client) =>
+    client.post(
+      "/addresses",
+      {
+        company: "Lob (old)",
+        address_line1: "185 Berry St",
+        address_line2: "# 6100",
+        address_city: "San Francisco",
+        address_state: "CA",
+        address_zip: "94107",
+        address_country: "US",
+      },
+      { headers: prism.authHeader }
+    )
+  );
+  t.equal(response.status, 200);
+  const to = response.data.id;
+
+  response = await prism.setup().then((client) =>
+    client.post(
+      "/addresses",
+      {
+        company: "Lob (new)",
+        address_line1: "210 King St",
+        address_city: "San Francisco",
+        address_state: "CA",
+        address_zip: "94107",
+        address_country: "US",
+      },
+      { headers: prism.authHeader }
+    )
+  );
+  t.equal(response.status, 200);
+  const from = response.data.id;
+
+  // create
+  response = await prism.setup().then((client) =>
     client.post(
       resource_endpoint,
       {
-        // your writable properties here
+        to: to,
+        from: from,
+        color: true,
+        //        send_date: new Date(),
+        file:
+          "https://s3-us-west-2.amazonaws.com/public.lob.com/assets/us_letter_1pg.pdf",
       },
       { headers: prism.authHeader }
     )
   );
   t.equal(response.status, 200);
 
-  // read, replace, update and delete created endpoint
+  // read and cancel created endpoint
   const ltr_endpoint = `${resource_endpoint}/${response.data.id}`;
 
   // list
@@ -42,13 +87,27 @@ test("create, list, read, replace, update, then delete a letter", async function
     .then((client) => client.get(ltr_endpoint, { headers: prism.authHeader }));
   t.equal(response.status, 200);
 
-  // cancel
+  /*  // cancel
   response = await prism
     .setup()
     .then((client) =>
       client.delete(ltr_endpoint, { headers: prism.authHeader })
     );
   t.equal(response.status, 200);
+
+  // delete addresses, too
+  response = await prism
+    .setup()
+    .then((client) =>
+      client.delete(`/addresses/{to}`, { headers: prism.authHeader })
+    );
+  t.equal(response.status, 200);
+  response = await prism
+    .setup()
+    .then((client) =>
+      client.delete(`/addresses/{from}`, { headers: prism.authHeader })
+    );
+  t.equal(response.status, 200); */
 });
 
 // add any failure cases you need here

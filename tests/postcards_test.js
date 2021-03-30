d9785ef4676a2862716fb60ced8e3d701b02a6e1
Aditi <aditi@lob.com>
2021-03-30 12:09:07 -0700

    updated test to deal with local file upload

--- tests/postcards_test.js
+++ tests/postcards_test.js
@@ -6,6 +6,8 @@
 
 // standard setup, present in every test
 const test = require("tape");
+var fs = require("fs");
+const path = require("path");
 const Prism = require("./setup.js");
 
 // test specific data
@@ -93,6 +95,53 @@ test("create, list, read then cancel a postcard", async function (t) {
   await deleteAddress(from);
 });
 
+test("creates a postcard given a local filepath as the front", async function (t) {
+  function streamToString(stream) {
+    const chunks = [];
+    return new Promise((resolve, reject) => {
+      stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
+      stream.on("error", (err) => reject(err));
+      stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+    });
+  }
+
+  const files = fs.createReadStream(
+    path.resolve(
+      "tests/assets/customer-thank-you-postcard-template-4x6-front.html"
+    )
+  );
+  const frontBack = await streamToString(files);
+
+  const create = await prism.setup().then((client) =>
+    client.post(
+      resource_endpoint,
+      {
+        to: {
+          company: "Lob (old)",
+          address_line1: "185 Berry St",
+          address_line2: "# 6100",
+          address_city: "San Francisco",
+          address_state: "CA",
+          address_zip: "94107",
+        },
+        from: {
+          company: "Lob (new)",
+          address_line1: "210 King St",
+          address_city: "San Francisco",
+          address_state: "CA",
+          address_zip: "94107",
+          address_country: "US",
+        },
+        front: frontBack,
+        back: frontBack,
+      },
+      { headers: prism.authHeader }
+    )
+  );
+  // console.log(create)
+  t.equal(create.status, 200);
+});
+
 // select failure cases:
 test("throws errors when input is not validated", async function (t) {
   // errors when one of the required fields (state) is missing
@@ -115,10 +164,10 @@ test("throws errors when input is not validated", async function (t) {
           address_zip: "94107",
           address_country: "US",
         },
-        color: true,
-        extra_service: "certified",
-        file:
-          "https://s3-us-west-2.amazonaws.com/public.lob.com/assets/us_letter_1pg.pdf",
+        front:
+          "https://s3-us-west-2.amazonaws.com/public.lob.com/assets/templates/4x6_pc_template.pdf",
+        back:
+          "https://s3-us-west-2.amazonaws.com/public.lob.com/assets/templates/4x6_pc_template.pdf",
       },
       { headers: prism.authHeader }
     )
@@ -145,10 +194,10 @@ test("throws errors when input is not validated", async function (t) {
           address_zip: "94107",
           address_country: "US",
         },
-        color: true,
-        extra_service: "certified",
-        file:
-          "https://s3-us-west-2.amazonaws.com/public.lob.com/assets/us_letter_1pg.pdf",
+        front:
+          "https://s3-us-west-2.amazonaws.com/public.lob.com/assets/templates/4x6_pc_template.pdf",
+        back:
+          "https://s3-us-west-2.amazonaws.com/public.lob.com/assets/templates/4x6_pc_template.pdf",
       },
       { headers: prism.authHeader }
     )

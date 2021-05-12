ac10425f5832d1411c4a7af27b8974bb0ab6b69a
Aditi <aditi@lob.com>
2021-05-11 18:41:03 -0700

    blah

--- run_one.js
+++ run_one.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 const pkg = require("../../package.json");
 const util = require("util");
-// const exec = util.promisify(require("child_process").exec);
+const exec = util.promisify(require("child_process").exec);
 
 module.exports.oneTest = async function oneTest() {
   const validator = Joi.string()
@@ -11,16 +11,13 @@ module.exports.oneTest = async function oneTest() {
     let test = "./tests/";
     test += await Joi.compile(validator).validateAsync(process.argv[2]);
     const test_command = 'tape "' + test + '" | tap-spec';
-    util.promisify(require("child_process").exec)(
-      test_command,
-      function (err, stdout, stderr) {
-        if (err) {
-          console.error(JSON.stringify(err, null, 2));
-          return err.code;
-        }
-        console.log(stdout);
+    exec(test_command, function (err, stdout, stderr) {
+      if (err) {
+        console.error(JSON.stringify(err, null, 2));
+        return err.code;
       }
-    );
+      console.log(stdout);
+    });
   } catch (err) {
     console.error(JSON.stringify(err, null, 2));
     return err.code;

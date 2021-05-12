3b72006cda2ee6d33dd8a3968e05d410045ec085
Aditi <aditi@lob.com>
2021-05-11 18:13:02 -0700

    feat(one-test): adds capability to run one test

--- run_one.js
+++ run_one.js
@@ -1,14 +1,27 @@
+const Joi = require("joi");
 const pkg = require("../../package.json");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-test = process.argv[2];
-test_command = 'tape "' + test + '" | tap-spec';
-exec(test_command, function (err, stdout, stderr) {
-  if (err) {
-    // actually: send this to the right slack channel
-    console.error(err);
+module.exports.oneTest = async function oneTest() {
+  const validator = Joi.string()
+    .regex(/^[a-z_]*_test.js$/)
+    .required();
+  try {
+    let test = "./tests/";
+    test += await Joi.compile(validator).validateAsync(process.argv[2]);
+    const test_command = 'tape "' + test + '" | tap-spec';
+    exec(test_command, function (err, stdout, stderr) {
+      if (err) {
+        console.error(JSON.stringify(err, null, 2));
+        return err.code;
+      }
+      console.log(stdout);
+    });
+  } catch (err) {
+    console.error(JSON.stringify(err, null, 2));
     return err.code;
   }
-  console.log(stdout);
-});
+};
+
+this.oneTest();

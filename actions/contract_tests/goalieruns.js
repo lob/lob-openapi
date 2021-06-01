758ec3fd056b8c23d8ec140b63350c9b54d03b82
Aditi Ramaswamy <15935019+sudoku-lord@users.noreply.github.com>
2021-06-01 11:01:49 -0700

    disabled slack alerts for now (#101)
    * disabled slack alerts for now
    
    * update bundled spec, zero-dependency docs
    
    Co-authored-by: lobot <lobot@lob.com>

--- actions/contract_tests/goalieruns.js
+++ actions/contract_tests/goalieruns.js
@@ -19,43 +19,42 @@ module.exports.runTests = async function runTests() {
         test = test_set[resource_name];
         test_command = 'multi-tape "' + test + '" | tap-spec';
         exec(test_command, async function (err, stdout, stderr) {
-          if (err) {
-            try {
-              // send this to the right slack channel
-              // the start and end indices, as well as the length, are constant because the stacktrace always
-              // includes the same spots in the tape module (only the failing test itself may change)
-              const startIndex = stdout.indexOf(
-                "at Test.bound [as equal] (/github/workspace/node_modules/tape/lib/test.js:91:32)"
-              );
-              const endIndex = stdout.indexOf(
-                "at processTicksAndRejections (internal/process/task_queues.js:93:5)"
-              );
-              const len = "at Test.bound [as equal] (/github/workspace/node_modules/tape/lib/test.js:91:32)"
-                .length;
-              const testMessage = stdout.slice(startIndex + len, endIndex);
-              const expectedCode = stdout.slice(
-                stdout.indexOf("expected: ") + 10,
-                stdout.indexOf("      actual:")
-              );
-              const actualCode = stdout.slice(
-                stdout.indexOf("actual:   ") + 10,
-                stdout.indexOf("      at:")
-              );
-              const block = [
-                `_FAILED CONTRACT TEST_:      ${testMessage}`,
-                `_EXPECTED_:  ${expectedCode}`,
-                `_ACTUAL_:      ${actualCode}`,
-              ];
-              const result = await web.chat.postMessage({
-                channel: pkg.config.goalieMappings[validated_arg].slackChannel,
-                text: `:sadpanda: ${block.join("\n")}`,
-              });
-            } catch (error) {
-              console.error(error);
-              return error.code;
-            }
-          }
-          console.log(stdout);
+          // if (err) {
+          //   try {
+          //     // send this to the right slack channel
+          //     // the start and end indices, as well as the length, are constant because the stacktrace always
+          //     // includes the same spots in the tape module (only the failing test itself may change)
+          //     const startIndex = stdout.indexOf(
+          //       "at Test.bound [as equal] (/github/workspace/node_modules/tape/lib/test.js:91:32)"
+          //     );
+          //     const endIndex = stdout.indexOf(
+          //       "at processTicksAndRejections (internal/process/task_queues.js:93:5)"
+          //     );
+          //     const len = "at Test.bound [as equal] (/github/workspace/node_modules/tape/lib/test.js:91:32)"
+          //       .length;
+          //     const testMessage = stdout.slice(startIndex + len, endIndex);
+          //     const expectedCode = stdout.slice(
+          //       stdout.indexOf("expected: ") + 10,
+          //       stdout.indexOf("      actual:")
+          //     );
+          //     const actualCode = stdout.slice(
+          //       stdout.indexOf("actual:   ") + 10,
+          //       stdout.indexOf("      at:")
+          //     );
+          //     const block = [
+          //       `_FAILED CONTRACT TEST_:      ${testMessage}`,
+          //       `_EXPECTED_:  ${expectedCode}`,
+          //       `_ACTUAL_:      ${actualCode}`,
+          //     ];
+          //     const result = await web.chat.postMessage({
+          //       channel: pkg.config.goalieMappings[validated_arg].slackChannel,
+          //       text: `:sadpanda: ${block.join("\n")}`,
+          //     });
+          //   } catch (error) {
+          //     console.error(error);
+          //     return error.code;
+          //   }
+          // }
         });
       }
     } catch (joiError) {

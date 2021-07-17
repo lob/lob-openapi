b52bc2b4f79c7ed6275429a86e0fe801e4ca7bde
Jessica Ho <45194378+jho44@users.noreply.github.com>
2021-07-16 18:06:06 -0700

    feat(urlencoded): add urlencoded tests (#131)
    * feat(urlencoded): add urlencoded tests
    
    * add test to checks
    
    Co-authored-by: Jessica Ho <jessica-ho@proxy-nlb-prod-us-west-2-v5-ac4e52c97755301b.elb.us-west-2.amazonaws.com>
    Co-authored-by: Aditi Ramaswamy <15935019+sudoku-lord@users.noreply.github.com>

--- tests/checks_test.js
+++ tests/checks_test.js
@@ -11,6 +11,13 @@ const resource_endpoint = "/checks",
 
 const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN);
 
+const payload = {
+  description: "Demo Check",
+  amount: 101.01,
+  memo: "poodles",
+  message: "End Racism",
+};
+
 // TODO: add full payload req
 test.serial.before(
   "create & validate verified bank account, and create & read a check",
@@ -48,7 +55,7 @@ test.serial.before(
       client.post(
         resource_endpoint,
         {
-          description: "Demo Check",
+          ...payload,
           to: {
             company: "Lob (old)",
             address_line1: "185 Berry St",
@@ -67,9 +74,6 @@ test.serial.before(
             address_country: "US",
           },
           bank_account: t.context.bank_id,
-          amount: 101.01,
-          memo: "poodles",
-          message: "End Racism",
         },
         { headers: prism.authHeader }
       )
@@ -85,6 +89,88 @@ test.serial.before(
   }
 );
 
+test.serial.before(
+  "create & validate verified bank account, and create & read a check urlencoded",
+  async function (t) {
+    t.plan(3);
+    // We need a verified bank account to create a check, so let's make that first
+    let result = await prism.setup().then((client) =>
+      client.post(
+        "/bank_accounts",
+        {
+          description: "Test Bank Account",
+          routing_number: "322271627",
+          account_number: "123456789",
+          signatory: "Jane Doe",
+          account_type: "individual",
+        },
+        { headers: prism.authHeader }
+      )
+    );
+
+    t.context.bank_id2 = result.data.id;
+
+    const verify = await prism
+      .setup()
+      .then((client) =>
+        client.post(
+          `/bank_accounts/${t.context.bank_id2}/verify`,
+          { amounts: [42, 12] },
+          { headers: prism.authHeader }
+        )
+      );
+    t.assert(verify.status === 200);
+
+    let body = new URLSearchParams({
+      ...payload,
+      bank_account: t.context.bank_id2,
+    });
+
+    const deepObjs = {
+      to: {
+        company: "Lob (old)",
+        address_line1: "185 Berry St",
+        address_line2: "# 6100",
+        address_city: "San Francisco",
+        address_state: "CA",
+        address_zip: "94107",
+        address_country: "US",
+      },
+      from: {
+        company: "Lob (new)",
+        address_line1: "210 King St",
+        address_city: "San Francisco",
+        address_state: "CA",
+        address_zip: "94107",
+        address_country: "US",
+      },
+    };
+    for (const key in deepObjs) {
+      Object.entries(deepObjs[key]).forEach(([innerKey, innerVal]) => {
+        body.append(`${key}[${innerKey}]`, `${innerVal}`);
+      });
+    }
+    body = body.toString();
+
+    t.context.create2 = await prism.setup({ errors: false }).then((client) =>
+      client.post(resource_endpoint, body, {
+        headers: {
+          ...prism.authHeader,
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+      })
+    );
+    t.assert(t.context.create2.status === 200);
+
+    t.context.read2 = await prism.setup().then((client) =>
+      client.get(`${resource_endpoint}/${t.context.create2.data.id}`, {
+        headers: prism.authHeader,
+      })
+    );
+    t.assert(t.context.read2.status === 200);
+  }
+);
+
 test("list check", async function (t) {
   const list = await prism
     .setup()
@@ -94,7 +180,7 @@ test("list check", async function (t) {
   t.assert(list.status === 200);
 });
 
-test.after.always("delete a check", async function (t) {
+test.after.always("delete all checks", async function (t) {
   const remove = await prism.setup().then((client) =>
     client.delete(`${resource_endpoint}/${t.context.read.data.id}`, {
       headers: prism.authHeader,
@@ -103,6 +189,18 @@ test.after.always("delete a check", async function (t) {
   t.assert(remove.status === 200);
   prism.setup().then((client) =>
     client.delete(`/bank_accounts/${t.context.bank_id}`, {
+      headers: prism.authHeader,
+    })
+  );
+
+  const remove2 = await prism.setup().then((client) =>
+    client.delete(`${resource_endpoint}/${t.context.read2.data.id}`, {
+      headers: prism.authHeader,
+    })
+  );
+  t.assert(remove2.status === 200);
+  prism.setup().then((client) =>
+    client.delete(`/bank_accounts/${t.context.bank_id2}`, {
       headers: prism.authHeader,
     })
   );

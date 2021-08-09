b4230dcfc84f9498d80a80843518f72128694cf4
Jessica Ho <hojessica1230@gmail.com>
2021-08-09 16:38:44 -0700

    webpack chunks

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+var paths = {
+  sassFiles: [
+    "./styles/main.scss",
+    "./styles/prettify.scss",
+    "./styles/rebrand-nav-footer.scss",
+  ],
+  dist: "./docs/chunks",
+  styleFiles: "./styles/**/*.scss",
+};
+
+module.exports = {
+  entry: {
+    blah: [
+      "./js/main.js",
+      paths.sassFiles[0],
+      paths.sassFiles[1],
+      paths.sassFiles[2],
+    ],
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+  output: {
+    path: path.resolve(__dirname, paths.dist),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.min.css",
+    }),
+  ],
+};

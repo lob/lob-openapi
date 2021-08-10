28856d3ce93a05ac33c979c2fca1edf7271b133e
Aditi <aditi@lob.com>
2021-08-10 13:51:48 -0700

    fixed up last bits of billing groups

--- tests/billing_groups_test.js
+++ tests/billing_groups_test.js
@@ -14,84 +14,69 @@ const payload = {
   name: "OpenAPI Test",
   description: "OpenAPI testing for Billing Groups",
 };
-let create = null;
 
 // contract tests happy path
-test("create, list, read, replace, update, then delete a billing_group", async function (t) {
+test.serial.before("create a billing_group", async function (t) {
+  t.plan(3);
   // JSON.stringify(slackError, null, 2)
-  try {
-    create = await prism
-    .setup()
+  const create = await prism
+    .setup({ errors: false })
     .then((client) =>
       client.post(resource_endpoint, payload, { headers: prism.authHeader })
     );
-    t.assert(create.status === 200);
-    t.assert(create.data.name === payload.name);
-    t.assert(create.data.description === payload.description);
-  } catch (err) {
-    console.log(JSON.stringify(err, null, 2));
-  }
+  t.assert(create.status === 200);
+  t.assert(create.data.name === payload.name);
+  t.assert(create.data.description === payload.description);
 
+  t.context.create_id = create.data.id;
+});
+
+test.serial.before("list billing_groups", async function (t) {
+  t.plan(1);
   const list = await prism
-    .setup()
+    .setup({ errors: false })
     .then((client) =>
       client.get(resource_endpoint, { headers: prism.authHeader })
     );
   t.assert(list.status === 200);
+});
 
-  const read = await prism.setup().then((client) =>
-    client.get(`${resource_endpoint}/${create.data.id}`, {
+test.serial.before("read a billing_group", async function (t) {
+  t.plan(3);
+  const read = await prism.setup({ errors: false }).then((client) =>
+    client.get(`${resource_endpoint}/${t.context.create_id}`, {
       headers: prism.authHeader,
     })
   );
   t.assert(read.status === 200);
-  t.assert(create.data.name === payload.name);
-  t.assert(create.data.description === payload.description);
+  t.assert(read.data.name === payload.name);
+  t.assert(read.data.description === payload.description);
+});
 
-  const replace = await prism.setup().then((client) =>
+test.serial.before("update a billing_group", async function (t) {
+  t.plan(2);
+  const update = await prism.setup({ errors: false }).then((client) =>
     client.post(
-      `${resource_endpoint}/${create.data.id}`,
+      `${resource_endpoint}/${t.context.create_id}`,
       {
         name: "Update Open Api Test",
       },
       { headers: prism.authHeader }
     )
   );
-  t.assert(replace.status === 200);
-  t.assert(replace.data.name === "Update Open Api Test");
+  t.assert(update.status === 200);
+  t.assert(update.data.name === "Update Open Api Test");
 });
 
 // add any failure cases you need here
-test("billing_group failure tests", async function (t) {
+test.serial("billing_group failure tests", async function (t) {
+  t.plan(1);
   // not sending required parameters
   const emptyCreatePayload = await prism
-    .setup()
+    .setup({ errors: false })
     .then((client) =>
       client.post(resource_endpoint, {}, { headers: prism.authHeader })
     );
   t.assert(emptyCreatePayload.status === 422);
-
-  // sending wrong params to create
-  const wrongParamCreate = await prism.setup().then((client) =>
-    client.post(
-      resource_endpoint,
-      {
-        foo: "bar",
-      },
-      { headers: prism.authHeader }
-    )
-  );
-  t.assert(wrongParamCreate.status === 422);
-
-  // sending wrong params to update
-  const replace = await prism.setup().then((client) =>
-    client.post(
-      `${resource_endpoint}/${create.data.id}`,
-      {
-        foo: "bar",
-      },
-      { headers: prism.authHeader }
-    )
-  );
-  t.assert(replace.status === 422);
+  // t.assert(emptyCreatePayload.status === 422);
 });

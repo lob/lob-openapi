a2af1e11c8ffe891d322de090c4a4dddbe56bae9
Aditi Ramaswamy <15935019+sudoku-lord@users.noreply.github.com>
2021-08-24 11:58:21 -0700

    added some stuff to contract tests (#225)

--- actions/contract_tests/goalieruns.js
+++ actions/contract_tests/goalieruns.js
@@ -42,9 +42,14 @@ module.exports.runTests = async function runTests() {
           // the errors being surfaced by ava are unexpected & don't come from the tests.
           if (noWhitespace.length > 1000) {
             try {
-              const generic_failure = await web.chat.postMessage({
+              const parent_devex = await web.chat.postMessage({
                 channel: "#devex-test-alerts",
-                text: failures[index],
+                text: `:woman-shrugging::skin-tone-4: An unexpected error surfaced in the contract tests. This does not originate from any Lob endpoint.`,
+              });
+              const reply_devex = await web.chat.postMessage({
+                channel: "#devex-test-alerts",
+                thread_ts: parent_devex.ts,
+                text: "```" + failures[index].replace(/```/g, " ") + "```",
               });
             } catch (slackError) {
               console.error(JSON.stringify(slackError, null, 2));

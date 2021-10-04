1b346ce5c34c76ab4a94ed34e832aba85218c9ad
Aditi <aditi@lob.com>
2021-10-04 08:19:23 -0700

    fixed some copypasta issues

--- cypress/integration/links_test.js
+++ cypress/integration/links_test.js
@@ -1,4 +1,4 @@
-describe("My First Test", () => {
+describe("ensures introduction links and text are valid", () => {
   beforeEach(() => {
     cy.visit("https://docs.lob.com/");
   });
@@ -24,7 +24,7 @@ describe("My First Test", () => {
   });
 
   it("contains the licence link", () => {
-    // makes sure the support link goes to the right page
+    // makes sure the licence link goes to the right page
     cy.get('*[class^="sc-gsWcmt gCgqqY"]')
       .contains("MIT")
       .should("have.attr", "href")
@@ -32,7 +32,7 @@ describe("My First Test", () => {
   });
 
   it("contains the terms of service link", () => {
-    // makes sure the support link goes to the right page
+    // makes sure the ToS link goes to the right page
     cy.get('*[class^="sc-gsWcmt gCgqqY"]')
       .contains("Terms of Service")
       .should("have.attr", "href")
@@ -40,7 +40,7 @@ describe("My First Test", () => {
   });
 
   it("contains the previous documentation link", () => {
-    // makes sure the support link goes to the right page
+    // makes sure the previous documentation link goes to the right page
     cy.get('*[class^="sc-iBzEeX sc-cOifOu dFWqin rUYTT"]')
       .contains("previous documentation")
       .should("have.attr", "href")
@@ -48,7 +48,7 @@ describe("My First Test", () => {
   });
 
   it("contains the HTTP Basic Auth link", () => {
-    // makes sure the support link goes to the right page
+    // makes sure the basic auth link goes to the right page
     cy.get('*[class^="sc-iBzEeX sc-cOifOu dFWqin rUYTT redoc-markdown"]')
       .contains("HTTP Basic authentication")
       .should("have.attr", "href")
@@ -59,7 +59,7 @@ describe("My First Test", () => {
   });
 
   it("contains the error codes link", () => {
-    // makes sure the support link goes to the right page
+    // makes sure the error codes link goes to the right page
     cy.get('*[class^="sc-iBzEeX sc-cOifOu dFWqin rUYTT redoc-markdown"]')
       .contains("error code")
       .should("have.attr", "href")
@@ -67,7 +67,7 @@ describe("My First Test", () => {
   });
 
   it("contains the dashboard link", () => {
-    // makes sure the support link goes to the right page
+    // makes sure the dashboard link goes to the right page
     cy.get('*[class^="sc-iBzEeX sc-cOifOu dFWqin rUYTT redoc-markdown"]')
       .contains("Dashboard Settings")
       .should("have.attr", "href")
@@ -75,7 +75,7 @@ describe("My First Test", () => {
   });
 
   it("contains the dashboard settings link again", () => {
-    // makes sure the support link goes to the right page
+    // makes sure the other dashboard link goes to the right page
     cy.get('*[class^="sc-iBzEeX sc-cOifOu dFWqin rUYTT redoc-markdown"]')
       .contains("Settings")
       .should("have.attr", "href")
@@ -83,12 +83,16 @@ describe("My First Test", () => {
   });
 
   it("contains the test & live environments link", () => {
-    // makes sure the support link goes to the right page
+    // makes sure the test & live environments link goes to the right page
     cy.get('*[class^="sc-iBzEeX sc-cOifOu dFWqin rUYTT redoc-markdown"]')
       .contains("Test and Live Environments")
       .should("have.attr", "href")
       .and("include", "#tag/Test-and-Live-Environments");
   });
-});
 
-// cy.contains("Lob’s Print & Mail and Address Verification APIs help companies transform outdated, manual print-and-mail processes; save 1,000s of hours in processing time by sending mail much more quickly; and increase ROI on offline communications.");
+  it("contains the right intro text", () => {
+    cy.contains(
+      "Lob’s Print & Mail and Address Verification APIs help companies transform outdated, manual print-and-mail processes; save 1,000s of hours in processing time by sending mail much more quickly; and increase ROI on offline communications."
+    );
+  });
+});

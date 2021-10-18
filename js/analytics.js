ee7cf7f0182cfc3403014ba8aa70efa98fe546ec
Aditi <aditi@lob.com>
2021-10-18 10:22:18 -0700

    changed the analytics file

--- js/analytics.js
+++ js/analytics.js
@@ -16,11 +16,12 @@ var dashToUnderscore = function (string) {
  * @returns {String}
  */
 var getUrlParams = function (sParam) {
-  var sPageURL = window.location.search.substring(1);
-  var sURLVariables = sPageURL.split("&");
-  for (var i = 0; i < sURLVariables.length; i++) {
-    var sParameterName = sURLVariables[i].split("=");
+  let sPageURL = window.location.search.substring(1);
+  let sURLVariables = sPageURL.split("&");
+  for (let i = 0; i < sURLVariables.length; i++) {
+    let sParameterName = sURLVariables[i].split("=");
     if (sParameterName[0] === sParam) {
+      console.log(sParameterName[1]);
       return sParameterName[1];
     }
   }

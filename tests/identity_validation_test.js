c0c441fbd20c828affc837530f64cd796e8d5c5d
Amaan Rahim <amaan@lob.com>
2022-07-18 14:04:48 -0700

    feat(av-3391): add identity_validation endpoint

---
+++ tests/identity_validation_test.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const test = require("ava");
+const Prism = require("./setup.js");
+
+// test specific data
+const resource_endpoint = "/identity_validation",
+  recipient = "Lob.com",
+  primary_line = "210 King St",
+  secondary_line = "",
+  city = "San Francisco",
+  state = "CA",
+  zip_code = "94107",
+  lobUri = "https://lob-api.lob.com/v1",
+  specFile = "./lob-api-public.yml";
+
+const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN);
+
+test("validate recipient and address", async function (t) {
+  try {
+    const response = await prism
+      .setup()
+      .then((client) =>
+        client.post(
+          resource_endpoint,
+          {
+            recipient: recipient,
+            primary_line: primary_line,
+            secondary_line: secondary_line,
+            city: city,
+            state: state,
+            zip_code: zip_code,
+          },
+          { headers: prism.authHeader }
+        )
+      );
+
+    t.assert(response.status === 200);
+  } catch (prismError) {
+    if (Object.keys(prismError).length > 0) {
+      t.fail(JSON.stringify(prismError, null, 2));
+    } else {
+      t.fail(prismError.toString());
+    }
+  }
+});
+
+test("absent recipient", async function (t) {
+  try {
+    const response = await prism
+      .setup({ errors: false })
+      .then((client) =>
+        client.post(
+          resource_endpoint,
+          {
+            recipient: recipient,
+            primary_line: primary_line,
+            secondary_line: secondary_line,
+            city: city,
+            state: state,
+            zip_code: zip_code,
+          },
+          { headers: prism.authHeader }
+        )
+      );
+
+    t.assert(response.status === 422);
+    t.assert(response.data.error.message.includes("recipient"));
+  } catch (prismError) {
+    if (Object.keys(prismError).length > 0) {
+      t.fail(JSON.stringify(prismError, null, 2));
+    } else {
+      t.fail(prismError.toString());
+    }
+  }
+});

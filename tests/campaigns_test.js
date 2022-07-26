d926d7c932250717596e111dc6e97f5c5c871c5b
Jessica Ho <45194378+jho44@users.noreply.github.com>
2022-07-25 17:29:33 -0700

    creatives+campaigns+uploads tests (#364)
    Co-authored-by: Aditi Ramaswamy <15935019+sudoku-lord@users.noreply.github.com>

--- tests/campaigns_test.js
+++ tests/campaigns_test.js
@@ -10,24 +10,22 @@ const resource_endpoint = "/campaigns",
   specFile = "./lob-api-public.yml";
 
 const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN);
-/* NOTE: DELETE ANY TESTS THAT DON'T APPLY TO YOUR RESOURCE */
 
-// contract tests happy path
-test("perform action on campaign", async function (t) {
+test.serial.before("create billing group", async function (t) {
   try {
-    t.context.create = await prism.setup().then((client) =>
+    const result = await prism.setup().then((client) =>
       client.post(
-        resource_endpoint,
+        "/billing_groups",
         {
-          // your editable properties here
+          name: "lob-openapi campaign billing group",
         },
         { headers: prism.authHeader }
       )
     );
-    t.assert(t.context.create.status === 200);
+
+    t.context.bg_id = result.data.id;
   } catch (prismError) {
     if (Object.keys(prismError).length > 0) {
-      // if the error has properties, it can be JSON.stringified
       t.fail(JSON.stringify(prismError, null, 2));
     } else {
       t.fail(prismError.toString());
@@ -35,6 +33,181 @@ test("perform action on campaign", async function (t) {
   }
 });
 
-// add any tests checking input variations here
+test("create, retrieve, update, then delete a campaign", async function (t) {
+  try {
+    // create
+    let response = await prism.setup().then((client) =>
+      client.post(
+        resource_endpoint,
+        {
+          name: "Harry Zhang",
+          schedule_type: "immediate",
+          billing_group_id: t.context.bg_id,
+          description: "lob-openapi Campaign description",
+          target_delivery_date: null,
+          send_date: null,
+          cancel_window_campaign_minutes: 60,
+          metadata: {
+            name: "Harry Zhang",
+          },
+          use_type: "operational",
+          auto_cancel_if_ncoa: false,
+        },
+        {
+          headers: { ...prism.authHeader, "x-lang-output": "match" },
+        }
+      )
+    );
 
-// add any failure cases you need here
+    t.assert(response.status === 200);
+
+    const cmp_id = response.data.id;
+
+    // retrieve
+    response = await prism.setup().then((client) =>
+      client.get(`${resource_endpoint}/${cmp_id}`, {
+        headers: prism.authHeader,
+      })
+    );
+
+    t.assert(response.status === 200);
+
+    // update
+    response = await prism.setup().then((client) =>
+      client.patch(
+        `${resource_endpoint}/${cmp_id}`,
+        {
+          name: "Harry II",
+          description: "lob-openapi Updated campaign",
+        },
+        {
+          headers: prism.authHeader,
+        }
+      )
+    );
+
+    t.assert(response.status === 200);
+    t.assert(response.data.description === "lob-openapi Updated campaign");
+
+    // delete
+    response = await prism.setup().then((client) =>
+      client.delete(`${resource_endpoint}/${cmp_id}`, {
+        headers: prism.authHeader,
+      })
+    );
+
+    t.assert(response.status === 200);
+  } catch (prismError) {
+    if (Object.keys(prismError).length > 0) {
+      t.fail(JSON.stringify(prismError, null, 2));
+    } else {
+      t.fail(prismError.toString());
+    }
+  }
+});
+
+test("list campaigns params", async function (t) {
+  const list = async (body) => {
+    try {
+      const response = await prism.setup().then((client) =>
+        client.get(`${resource_endpoint}?${body}`, {
+          headers: prism.authHeader,
+        })
+      );
+
+      t.assert(response.status === 200);
+      return response.data;
+    } catch (prismError) {
+      if (Object.keys(prismError).length > 0) {
+        t.fail(JSON.stringify(prismError, null, 2));
+      } else {
+        t.fail(prismError.toString());
+      }
+    }
+  };
+
+  /* ################## LIMIT ################## */
+
+  const limit_body = new URLSearchParams({ limit: 4 });
+  const limit_response = list(limit_body.toString());
+
+  /* ################## BEFORE ################## */
+
+  const before_body = new URLSearchParams({
+    before:
+      "eyJkYXRlT2Zmc2V0IjoiMjAyMS0wNy0yMVQxMjo0MDo0NS43NjNaIiwiaWRPZmZzZXQiOiJ0bXBsXzEwOWQ4YzZlZGIwNDgwOSJ9",
+  });
+
+  const before_response = list(before_body.toString());
+
+  /* ################## AFTER ################## */
+
+  const after_body = new URLSearchParams({
+    after:
+      "eyJkYXRlT2Zmc2V0IjoiMjAyMS0wNy0yMVQxMjo0MDo0NS43NjNaIiwiaWRPZmZzZXQiOiJ0bXBsXzEwOWQ4YzZlZGIwNDgwOSJ9",
+  });
+
+  const after_response = list(after_body.toString());
+
+  /* ################## INCLUDE ################## */
+
+  const include_body = new URLSearchParams({ "include[]": "total_count" });
+  const include_response = list(include_body.toString());
+
+  /* ################## FULL ################## */
+
+  const full_body = new URLSearchParams({
+    limit: 2,
+    after:
+      "eyJkYXRlT2Zmc2V0IjoiMjAyMS0wNy0yMVQxMjo0MDo0NS43NjNaIiwiaWRPZmZzZXQiOiJ0bXBsXzEwOWQ4YzZlZGIwNDgwOSJ9",
+    "include[]": "total_count",
+  });
+
+  const full_response = list(full_body.toString());
+
+  /* ################## RUN EVERYTHING ASYNC ################## */
+
+  try {
+    const finale = await Promise.all([
+      limit_response,
+      before_response,
+      after_response,
+      include_response,
+      full_response,
+    ]);
+
+    t.assert(finale[0].count <= 4);
+    t.assert(finale[3].hasOwnProperty("total_count"));
+    t.assert(finale[4].hasOwnProperty("total_count"));
+    t.assert(finale[4].count <= 2);
+  } catch (prismError) {
+    if (Object.keys(prismError).length > 0) {
+      t.fail(JSON.stringify(prismError, null, 2));
+    } else {
+      t.fail(prismError.toString());
+    }
+  }
+});
+
+test("errors when schedule_type is not passed in", async function (t) {
+  try {
+    const response = await prism.setup({ errors: false }).then((client) =>
+      client.post(
+        resource_endpoint,
+        {
+          name: "lob-openapi failed campaign",
+        },
+        { headers: prism.authHeader }
+      )
+    );
+
+    t.assert(response.status === 422);
+    t.assert(response.data.error.message.includes("schedule_type"));
+  } catch (prismError) {
+    if (Object.keys(prismError).length > 0) {
+      t.fail(JSON.stringify(prismError, null, 2));
+    } else {
+      t.fail(prismError.toString());
+    }
+  }
+});
--- tests/creatives_test.js
+++ tests/creatives_test.js
@@ -3,6 +3,7 @@
 // standard setup, present in every test
 const test = require("ava");
 const Prism = require("./setup.js");
+const fs = require("fs");
 
 // test specific data
 const resource_endpoint = "/creatives",
@@ -10,24 +11,23 @@ const resource_endpoint = "/creatives",
   specFile = "./lob-api-public.yml";
 
 const prism = new Prism(specFile, lobUri, process.env.LOB_API_TEST_TOKEN);
-/* NOTE: DELETE ANY TESTS THAT DON'T APPLY TO YOUR RESOURCE */
 
-// contract tests happy path
-test("perform action on creative", async function (t) {
+test.serial.before("create campaign", async function (t) {
   try {
-    t.context.create = await prism.setup().then((client) =>
+    const result = await prism.setup().then((client) =>
       client.post(
-        resource_endpoint,
+        "/campaigns",
         {
-          // your editable properties here
+          name: "lob-openapi creatives campaign",
+          schedule_type: "immediate",
         },
         { headers: prism.authHeader }
       )
     );
-    t.assert(t.context.create.status === 200);
+
+    t.context.cmp_id = result.data.id;
   } catch (prismError) {
     if (Object.keys(prismError).length > 0) {
-      // if the error has properties, it can be JSON.stringified
       t.fail(JSON.stringify(prismError, null, 2));
     } else {
       t.fail(prismError.toString());
@@ -35,6 +35,116 @@ test("perform action on creative", async function (t) {
   }
 });
 
-// add any tests checking input variations here
+test("create, retrieve, and update a creative", async function (t) {
+  try {
+    // create
+    let response = await prism.setup().then((client) =>
+      client.post(
+        resource_endpoint,
+        {
+          campaign_id: "cmp_60c33dc5d6674b22",
+          resource_type: "letter",
+          details: {
+            color: true,
+          },
+          description: "lob-openapi creative",
+          from: {
+            company: "Lob (old)",
+            address_line1: "210 King St",
+            address_line2: "# 6100",
+            address_city: "San Francisco",
+            address_state: "CA",
+            address_zip: "94107",
+            address_country: "US",
+          },
+          file: fs.readFile("tests/assets/us_letter_1pg.pdf", (err, data) => {
+            if (err) throw err;
+            return data;
+          }),
+          metadata: {
+            name: "Harry Zhang",
+          },
+        },
+        {
+          headers: { ...prism.authHeader, "x-lang-output": "match" },
+        }
+      )
+    );
 
-// add any failure cases you need here
+    t.assert(response.status === 200);
+
+    const crv_id = response.data.id;
+
+    // retrieve
+    response = await prism.setup().then((client) =>
+      client.get(`${resource_endpoint}/${crv_id}`, {
+        headers: prism.authHeader,
+      })
+    );
+
+    t.assert(response.status === 200);
+
+    // update
+    response = await prism.setup().then((client) =>
+      client.patch(
+        `${resource_endpoint}/${crv_id}`,
+        {
+          description: "lob-openapi Updated creative",
+        },
+        {
+          headers: prism.authHeader,
+        }
+      )
+    );
+
+    t.assert(response.status === 200);
+    t.assert(response.data.description === "lob-openapi Updated creative");
+  } catch (prismError) {
+    if (Object.keys(prismError).length > 0) {
+      t.fail(JSON.stringify(prismError, null, 2));
+    } else {
+      t.fail(prismError.toString());
+    }
+  }
+});
+
+test("errors when details is not passed in", async function (t) {
+  try {
+    const response = await prism.setup({ errors: false }).then((client) =>
+      client.post(
+        resource_endpoint,
+        {
+          campaign_id: t.context.cmp_id,
+          resource_type: "letter",
+        },
+        { headers: prism.authHeader }
+      )
+    );
+
+    t.assert(response.status === 422);
+    t.assert(response.data.error.message.includes("details"));
+  } catch (prismError) {
+    if (Object.keys(prismError).length > 0) {
+      t.fail(JSON.stringify(prismError, null, 2));
+    } else {
+      t.fail(prismError.toString());
+    }
+  }
+});
+
+test.after.always("delete campaign", async function (t) {
+  try {
+    const deleted_campaign = await prism.setup().then((client) =>
+      client.delete(`/campaigns/${t.context.cmp_id}`, {
+        headers: prism.authHeader,
+      })
+    );
+    t.assert(deleted_campaign.status === 200);
+  } catch (prismError) {
+    if (Object.keys(prismError).length > 0) {
+      t.fail(JSON.stringify(prismError, null, 2));
+    } else {
+      t.fail(prismError.toString());
+    }
+  }
+});

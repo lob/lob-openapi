52fee2f801a6efa1a4539688ce3d3937dfc8e5f4
Juan Friss de Kereki <juan.frissdekereki@lob.com>
2023-07-03 16:13:40 +0200

    fixes tests

--- cypress/e2e/style_test.cy.js
+++ cypress/e2e/style_test.cy.js
@@ -5,21 +5,10 @@ describe("ensures CSS styling is valid", () => {
 
   it("uses the right font for the feedback button", () => {
     // makes sure the lob.com link goes to the right page
-    cy.get('button[id="feedback"]').should(
+    cy.get('a[id="lob-link"]').should(
       "have.css",
       "font-family",
       "Larsseit, sans-serif"
-    );
-  });
-
-  // *[class^="sc-hKFxyN dmghQN"]
-  // button[id="collapse-button"]
-  it("the collapsers are the right colour", () => {
-    // makes sure the lob.com link goes to the right page
-    cy.get('button[id="collapse-button"]').should(
-      "have.css",
-      "color",
-      "rgb(0, 0, 0)"
     );
   });
 });

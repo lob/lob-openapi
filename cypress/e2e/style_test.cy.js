4aaf23e3a47e23c1340b5879928441a1f8e6d90e
Karrissa <karrissa.volcy@lob.com>
2026-05-29 19:43:16 -0400

    FD-881: Remove stale docs style test comment

--- cypress/e2e/style_test.cy.js
+++ cypress/e2e/style_test.cy.js
@@ -4,7 +4,6 @@ describe("ensures CSS styling is valid", () => {
   });
 
   it("uses the right font for the Lob brand link", () => {
-    // makes sure the lob.com link goes to the right page
     cy.get('a[id="lob-link"]').should(
       "have.css",
       "font-family",
